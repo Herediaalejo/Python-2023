b8489547822131daa7937d2b6fb8a0993f65f900
Herediaalejo <herediaalejodeveloper@gmail.com>
2023-06-20 21:11:36 -0300

    elimino archivo prueba

--- ejerciciosGeneral/Untitled-1.py
+++
@@ -1,8 +0,0 @@
-a = [["a","asdaasdass","efdsfsf"],["abd","efdfsdfsd","fdkmsnfdksfmdskfmdskfm"]]
-
-""" for i in range(len(a)):
-    print(max(len(str(a[i][0])))) """
-
-b = ["abs","abfdgd"] 
-
-print(max(b))
