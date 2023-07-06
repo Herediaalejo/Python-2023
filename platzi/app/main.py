7df0f398f2f3f7a43039ec20b77748398855cd9f
Herediaalejo <herediaalejodeveloper@gmail.com>
2023-07-06 20:35:48 -0300

    ultimas modificaciones

--- platzi/app/main.py
+++ platzi/app/main.py
@@ -4,14 +4,41 @@ import charts
 
 def run():
     data = rc.read_csv("./app/data.csv")
-    country = input("Introduzca el país => ")
+    op = int(input("Introduzca la opcion que desea realizar\n1) Poblacion historia de un pais\n2) Porcentaje de poblacion mundial\n:"))
+    if op == 1:
+        country = input("Introduzca el país => ")
+        result = utils.population_by_country(data,country)
 
-    result = utils.population_by_country(data,country)
+        if len(result)>0:
+            country_data = result 
+            labels, values = utils.get_population(country_data)
+            charts.generate_bar_chart(labels, values)
+    elif op == 2 :
+        op = int(input("¿Desea filtrar por region u obtener el global?\n1) Por continente\n2) Global\n:"))
+        if op == 1:
+            op = int(input("Continentes\n1) America del sur\n2) America del norte\n3) Europa\n4) Asia\n5) Africa\n6) Oceania\n:")) 
+            if op == 1:
+                data_continent = utils.population_by_continent(data, "South America")
 
-    if len(result)>0:
-        country_data = result 
-        labels, values = utils.get_population(country_data)
-        charts.generate_bar_chart(labels, values)
+            elif op == 2:
+                data_continent = utils.population_by_continent(data, "North America")
+
+            elif op == 3:
+                data_continent = utils.population_by_continent(data, "Europe")
+
+            elif op == 4:
+                data_continent = utils.population_by_continent(data, "Asia")
+
+            elif op == 5:
+                data_continent = utils.population_by_continent(data, "Africa")
+
+            elif op == 6:
+                data_continent = utils.population_by_continent(data, "Oceania")
+        
+            labels,values = utils.world_population(data_continent)
+        else:
+            labels,values = utils.world_population(data)
+        charts.generate_pie_chart(labels, values)
 
 if __name__ == "__main__":
     run() # Entry point
--- platzi/app/utils.py
+++ platzi/app/utils.py
@@ -3,9 +3,27 @@ def population_by_country(data, country):
     result = list(filter(lambda item: item["Country/Territory"] == country, data))
     return(result)
 
+def population_by_continent(data, continent):
+    continent = continent.title()
+    result = list(filter(lambda item: item["Continent"] == continent, data))
+    return(result)
+
 def get_population(data_country):
-    population = {key: int(value) for key, value in data_country[0].items() if "Population" in key and not "World Population Percentage" in key}
+    population = {key.replace(" Population",""): int(value) for key, value in data_country[0].items() if "Population" in key and not "World Population Percentage" in key}
     labels = list(population.keys())
     values = list(population.values())
     return labels, values
+
+def world_population(data):
+    country = [data[i]["Country/Territory"] for i in range(len(data))]
+    #country = list(map(lambda x:x["Country/Territory"], data))
+    world_por = [data[i]["World Population Percentage"] for i in range(len(data))]
+    #world_por = list(map(lambda x:x["World Population Percentage"], data))
+    return country, world_por
+
+def continent_population(data):
+    country = [data[i]["Country/Territory"] for i in range(len(data))]
+    world_por = [data[i]["World Population Percentage"] for i in range(len(data))]
+    return country, world_por
+
 
