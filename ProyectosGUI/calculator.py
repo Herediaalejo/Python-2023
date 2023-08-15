1cdad270d239074ad57c044193b74d0f9abbe830
Herediaalejo <herediaalejodeveloper@gmail.com>
2023-08-15 20:42:42 -0300

    version 1.0

--- ProyectosGUI/calculator.py
+++ ProyectosGUI/calculator.py
@@ -2,32 +2,55 @@ from tkinter import *
 
 window = Tk()
 window.title("Calculator")
+window.configure(bg="#1d1f1e")
+window.resizable(0,0)
 
-num_input = Text(window, state="disabled", width=35, height=3, background="black", foreground="white", font=("Helvetica",15))
-num_input.grid(row=0,column=0,columnspan=4, padx=5, pady=5)
+def calculate(operation):
+    if "x" in operation:
+        operation=operation.replace("x","*")
+    elif "÷" in operation:
+        operation=operation.replace("÷", "/")
+    try:
+        result = eval(operation)
+        return result
+    except:
+        return ("Error de sintaxis")
 
-button1 = Button(window, bg="gray", fg="white",text="1", width=6, height=2, font=("Segoe UI",15))
-button2 = Button(window, bg="gray", fg="white",text="2", width=6, height=2, font=("Segoe UI",15))
-button3 = Button(window, bg="gray", fg="white",text="3", width=6, height=2, font=("Segoe UI",15))
-button4 = Button(window, bg="gray", fg="white",text="4", width=6, height=2, font=("Segoe UI",15))
-button5 = Button(window, bg="gray", fg="white",text="5", width=6, height=2, font=("Segoe UI",15))
-button6 = Button(window, bg="gray", fg="white",text="6", width=6, height=2, font=("Segoe UI",15))
-button7 = Button(window, bg="gray", fg="white",text="7", width=6, height=2, font=("Segoe UI",15))
-button8 = Button(window, bg="gray", fg="white",text="8", width=6, height=2, font=("Segoe UI",15))
-button9 = Button(window, bg="gray", fg="white",text="9", width=6, height=2, font=("Segoe UI",15))
-button0 = Button(window, bg="gray", fg="white",text="0", width=6, height=2, font=("Segoe UI",15))
+def get_result(input_camp, result):
+    input_camp.delete(0,END)
+    input_camp.insert(0,result)
 
-buttonAC = Button(window, bg="gray", fg="white",text="AC", width=6, height=2, font=("Segoe UI",15))
-buttonPar1 = Button(window, bg="gray", fg="white",text="(", width=6, height=2, font=("Segoe UI",15))
-buttonPar2 = Button(window, bg="gray", fg="white",text=")", width=6, height=2, font=("Segoe UI",15))
 
-buttonDiv = Button(window, bg="gray", fg="white",text="÷", width=6, height=2, font=("Segoe UI",15))
-buttonMult = Button(window, bg="gray", fg="white",text="x", width=6, height=2, font=("Segoe UI",15))
-buttonRest = Button(window, bg="gray", fg="white",text="-", width=6, height=2, font=("Segoe UI",15))
-buttonSum = Button(window, bg="gray", fg="white",text="+", width=6, height=2, font=("Segoe UI",15))
-buttonDec = Button(window, bg="gray", fg="white",text=".", width=6, height=2, font=("Segoe UI",15))
-buttonDel = Button(window, bg="gray", fg="white",text="⌫", width=6, height=2, font=("Segoe UI",15))
-buttonEq = Button(window, bg="gray", fg="white",text="=", width=6, height=2, font=("Segoe UI",15))
+
+
+
+
+num_input = Entry(window, cursor="arrow", width=32, background="#565c58", foreground="white", font=("Helvetica",18), justify="right")
+num_input.grid(row=0,column=0,columnspan=4, padx=5, pady=15, ipady=50)
+
+button_color = "#555956"
+button1 = Button(window, bg=button_color, cursor="hand2", fg="white",text="1", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "1"))
+button2 = Button(window, bg=button_color, cursor="hand2", fg="white",text="2", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "2"))
+button4 = Button(window, bg=button_color, cursor="hand2", fg="white",text="4", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "3"))
+button5 = Button(window, bg=button_color, cursor="hand2", fg="white",text="5", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "4"))
+button6 = Button(window, bg=button_color, cursor="hand2", fg="white",text="6", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "5"))
+button3 = Button(window, bg=button_color, cursor="hand2", fg="white",text="3", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "6"))
+button7 = Button(window, bg=button_color, cursor="hand2", fg="white",text="7", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "7"))
+button8 = Button(window, bg=button_color, cursor="hand2", fg="white",text="8", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "8"))
+button9 = Button(window, bg=button_color, cursor="hand2", fg="white",text="9", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "9"))
+button0 = Button(window, bg=button_color, cursor="hand2", fg="white",text="0", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "0"))
+
+buttonAC = Button(window, bg=button_color, cursor="hand2", fg="white",text="AC", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.delete(0, END))
+buttonPar1 = Button(window, bg=button_color, cursor="hand2", fg="white",text="(", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "("))
+buttonPar2 = Button(window, bg=button_color, cursor="hand2", fg="white",text=")", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, ")"))
+
+buttonSum = Button(window, bg=button_color, cursor="hand2", fg="white",text="+", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "+"))
+buttonRest = Button(window, bg=button_color, cursor="hand2", fg="white",text="-", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "-"))
+buttonMult = Button(window, bg=button_color, cursor="hand2", fg="white",text="x", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "x"))
+buttonDiv = Button(window, bg=button_color, cursor="hand2", fg="white",text="÷", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "÷"))
+buttonDec = Button(window, bg=button_color, cursor="hand2", fg="white",text=".", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.insert(END, "."))
+buttonDel = Button(window, bg=button_color, cursor="hand2", fg="white",text="⌫", width=6, height=2, font=("Segoe UI",15), command= lambda : num_input.delete(num_input.index(END)-1,END))
+buttonEq = Button(window, bg=button_color, cursor="hand2", fg="white",text="=", width=6, height=2, font=("Segoe UI",15), command= lambda : get_result(num_input, calculate(num_input.get())))
 
 
 buttonAC.grid(row=1,column=0, padx=1, pady=5)
@@ -50,6 +73,13 @@ button0.grid(row=5,column=0, padx=1, pady=5)
 buttonDec.grid(row=5,column=1, padx=1, pady=5)
 buttonDel.grid(row=5,column=2, padx=1, pady=5)
 buttonEq.grid(row=5,column=3, padx=1, pady=5)
+
+
+op = num_input.get()
+print(op)
+if len(op)>0:
+    num_input.delete(0,END)
+    num_input.insert(0,calculate(op))
 
 
 window.mainloop()
