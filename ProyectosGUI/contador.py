27ad6b6a0b86011f3162ca38b1de4c6be9571151
Alejo Manuel Heredia <HerediaAlejoDeveloper@gmail.com>
2023-08-23 08:46:05 -0300

    Add files via upload

--- ProyectosGUI/contador.py
+++ ProyectosGUI/contador.py
@@ -6,7 +6,7 @@ class Program():
     def __init__(self) -> None:
         self.window = Tk()
         self.window.geometry("430x100")
-        self.window.title("ContCreciente")
+        self.window.title("Contador")
         self.window.resizable(0,0)
         self.createWidgets()
         self.window.mainloop()
