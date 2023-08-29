a6d61baab98b81e890de293c8ca607eb6e98a1b5
Herediaalejo <herediaalejodeveloper@gmail.com>
2023-08-29 19:12:43 -0300

    ejercicio FINAL

--- ProyectosGUI/factorial.py
+++ ProyectosGUI/factorial.py
@@ -5,17 +5,28 @@ from tkinter import ttk
 class Program():
     def __init__(self) -> None:
         self.window = Tk()
-        self.window.geometry("600x100")
+        self.window.geometry("600x130")
         self.window.title("Factorial")
         self.window.resizable(0,0)
         self.window.config(bg="#A8FFB8")
         self.createWidgets()
         self.window.mainloop()
+
     
     def count_sum(self):
         self.count = self.num_inp.get()
         self.count = int(self.count)
         self.count+=1
+        self.num_inp.config(state=NORMAL)
+        self.num_inp.delete(0,END)
+        self.num_inp.insert(0, self.count)
+        self.num_inp.config(state="readonly")
+
+    def count_sub(self):
+        self.count = self.num_inp.get()
+        self.count = int(self.count)
+        if self.count!=1:
+            self.count-=1
         self.num_inp.config(state=NORMAL)
         self.num_inp.delete(0,END)
         self.num_inp.insert(0, self.count)
@@ -41,24 +52,27 @@ class Program():
         self.style.configure('TButton', font=('Segoe UI', 12, "bold"), background="#A8FFB8")
         self.style.configure('TEntry', background="#A8FFB8")
         self.num_label = ttk.Label(self.window, text="n")
-        self.num_label.grid(row=0, column=0, pady=30, padx=10)
+        self.num_label.grid(row=1, column=0, padx=10)
 
         self.num_inp = ttk.Entry(self.window, font=('Segoe UI', 12, "bold"), width=10)
         self.num_inp.insert(0, 1)
         self.num_inp.config(state="readonly")
-        self.num_inp.grid(row=0, column=1,pady=30, padx=10)
+        self.num_inp.grid(row=1, column=1, padx=10)
 
         self.fact_label = ttk.Label(self.window, text="Factorial(n)")
-        self.fact_label.grid(row=0, column=2, pady=30, padx=10)
+        self.fact_label.grid(row=1, column=2, padx=10)
 
         self.fact_inp = ttk.Entry(self.window, font=('Segoe UI', 12, "bold"), width=20)
         self.fact_inp.insert(0, 1)
         self.fact_inp.config(state="readonly")
-        self.fact_inp.grid(row=0, column=3,pady=30, padx=10)
+        self.fact_inp.grid(row=1, column=3, padx=10)
 
         self.sig_button = ttk.Button(self.window, cursor="hand2",text="Siguiente", width=10)
-        self.sig_button.grid(row=0,column=4,pady=30, padx=10)
+        self.sig_button.grid(row=0,column=4,pady=(15,0), padx=10)
         self.sig_button.config(command=lambda:[self.count_sum(), self.calc_fact(int(self.num_inp.get()))])
+
+        self.back_button = ttk.Button(self.window, cursor="hand2",text="Anterior", width=10, command=lambda:[self.count_sub(), self.calc_fact(int(self.num_inp.get()))])
+        self.back_button.grid(row=2,column=4, padx=10)
 
 
 def main():
