97b97ceb36739af1875c11f7ff285a125edea207
Alejo Manuel Heredia <HerediaAlejoDeveloper@gmail.com>
2023-10-09 13:18:53 -0300

    Segunda instancia de la trivia

--- ProyectosGUI (Tkinter)/Preguntas y Respuestas.py
+++ ProyectosGUI (Tkinter)/Preguntas y Respuestas.py
@@ -3,31 +3,62 @@ from tkinter import ttk
 from tkinter import messagebox
 import mysql.connector
 
-def crear_celda(ventana, row, column, width=100, height=30):
+
+def crear_celda(ventana, row, column, width, height=30):
     celda = tk.Frame(ventana, width=width,height=height, bg=bgcolor)#,borderwidth=1, relief='solid'
-    celda.grid(row=row, column=column, pady=(0,30))
+    celda.grid(row=row, column=column, pady=(0,30), padx=20)
     return celda
 
 root = tk.Tk()
 root.title("Trivia")
-root.attributes('-fullscreen', not root.attributes('-fullscreen'))
-root.resizable(0,0)
+root.geometry("{0}x{1}+0+0".format(root.winfo_screenwidth(), root.winfo_screenheight()))
+root.resizable(0, 0)
+screen_width = root.winfo_screenwidth()
 
 bgcolor = "#BCFFCC"
 frcolor = "#ADDAFF"
 
-for row in range(4):
-    for column in range(2):
-        crear_celda(root, row, column)
+for row in range(18):
+    for column in range(4):
+        crear_celda(root, row, column, (screen_width-180)/4)
 
 estilo = ttk.Style()
-estilo.configure("TLabel", font=("Arial", 40)) 
-
+estilo.configure("TLabel", font=("Arial", 30)) 
+estilo.configure("TEntry", font=("Arial", 20)) 
+estilo.configure('newGame.TButton', font=('Helvetica', 16))
 
 
 root.config(bg="white")
 # Iniciar el bucle principal de tkinter
 
 title = ttk.Label(text="TRIVIAUI", background=bgcolor)
-title.grid(row=0,column=0)
+title.grid(row=0,column=0, columnspan=2, rowspan=2)
+
+name_label = ttk.Label(text="NOMBRE *", background=bgcolor, font=("Arial", 20))
+name_label.grid(row=3, column=0, columnspan=2, rowspan=2)
+
+name_entry = ttk.Entry(width=30, font=("Arial", 16))
+name_entry.grid(row=4,column=0, columnspan=2, rowspan=2)
+
+ig_label = ttk.Label(text="INSTAGRAM", background=bgcolor, font=("Arial", 20))
+ig_label.grid(row=5, column=0, columnspan=2, rowspan=2)
+
+ig_entry = ttk.Entry(width=30, font=("Arial", 16))
+ig_entry.grid(row=6,column=0, columnspan=2, rowspan=2)
+
+tel_label = ttk.Label(text="TELEFONO", background=bgcolor, font=("Arial", 20))
+tel_label.grid(row=7, column=0, columnspan=2, rowspan=2)
+
+tel_entry = ttk.Entry(width=30, font=("Arial", 16))
+tel_entry.grid(row=8,column=0, columnspan=2, rowspan=2)
+
+newGame_button = ttk.Button(root, width=20, text="NUEVO JUEGO", style='newGame.TButton')
+newGame_button.grid(row=9,column=0, columnspan=2, rowspan=2)
+
+punt_label = ttk.Label(root, text="PUNTAJE", background=bgcolor)
+punt_label.grid(row=0, column=2, columnspan=2, rowspan=2)
+
+punt_tree = ttk.Treeview(root)
+punt_tree.grid(row=2, column=2, columnspan=2, rowspan=6)
+
 tk.mainloop()
