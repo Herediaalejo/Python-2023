4e3e70d91270d920f595ce35b11f0e633c1315da
Alejo Manuel Heredia <HerediaAlejoDeveloper@gmail.com>
2023-10-29 21:18:31 -0300

    actualizo juego

--- ProyectosGUI (Tkinter)/trivia.py
+++ ProyectosGUI (Tkinter)/trivia.py
@@ -5,7 +5,7 @@ from tkinter import messagebox
 from tkinter import font
 import mysql.connector
 import time
-from PIL import Image, ImageTk
+from PIL import Image, ImageTk 
 import random
 
 
@@ -32,21 +32,54 @@ def reiniciar_tiempo(label):
     tiempo_transcurrido = 0
     label.config(text="00:00")
 
+def min_to_sec(tiempo):
+  minutos, segundos = tiempo.split(":")
+  minutos = int(minutos)
+  segundos = int(segundos)
+
+  segundos = minutos * 60 + segundos
+
+  return segundos
+
+def sec_to_min(segundos):
+
+  minutos = segundos // 60 #division entera, devuelve numero entero
+  segundos_restantes = segundos % 60 #el resto de la division
+  minutos = int(minutos)
+  segundos_restantes = int(segundos_restantes)
+
+  tiempo = f"{minutos:02}:{segundos_restantes:02}" #02 hace que el numero se vea con dos digitos por mas que tenga uno solo, es decir agrega un 0 a la izquierda cuando hay tan solo un caracter
+
+  return tiempo
+
 # Función para cargar y mostrar información en el Treeview
 def cargar_jugadores(tree):
     tree.delete(*tree.get_children())  # Borrar datos existentes en el Treeview
     cursor = conexion.cursor()
     cursor.execute("SELECT NOMBRE, TIEMPO, PUNTAJE FROM JUGADOR")
-    puntajes = cursor.fetchall()
-
-    for row in puntajes:
+    datos = cursor.fetchall()
+    jugadores = []
+    for jugador in datos:
+        jugadores.append(list(jugador))
+    for i in range(len(jugadores)):
+        jugadores[i][1] = sec_to_min(jugadores[i][1])
+    for row in jugadores:
         tree.insert("", "end", values=row)
+    cursor.close()
 
 def cargar_preguntas():
     cursor = conexion.cursor()
     cursor.execute("SELECT ID_PREGUNTA, NOMBRE, RESPUESTA_CORRECTA, RESPUESTA_INCORRECTA1, RESPUESTA_INCORRECTA2, RESPUESTA_INCORRECTA3 FROM PREGUNTA")
     preguntas = cursor.fetchall()
+    cursor.close()
     return preguntas
+
+def guardar_jugador():
+    cursor = conexion.cursor()
+    tiempo = min_to_sec(tiempo_logrado)
+    cursor.execute("INSERT INTO JUGADOR (NOMBRE, INSTAGRAM, TELEFONO, PUNTAJE, TIEMPO) values (%s,%s,%s,%s,%s)", (nombre, instagram, telefono, puntaje, tiempo))
+    conexion.commit()
+    cursor.close()
 
 def seleccionar_pregunta():
     if not listaPreguntas:
@@ -80,12 +113,22 @@ def insertar_salto_de_linea(texto, limite):
     return ' '.join(palabras)
 
 def sumar_puntos():
-    global puntaje
+    global puntaje, totalAcertado
     puntaje+=100
+    totalAcertado +=1
 
 def nueva_pregunta():
     pregunta_seleccionada = seleccionar_pregunta()
     if pregunta_seleccionada:
+        rojo = "#f54254"
+        azul = "#4287f5"
+        verde = "#42f57b"
+        amarillo = "#f5e342"
+        button_color = [rojo, azul, verde, amarillo]
+        random.shuffle(button_color)
+        #fila, columna, padx(primervalor), padx(segundovalor)
+        button_grid = [(9,1,0,50),(9,2,50,0),(12,1,0,50),(12,2,50,0)]
+        random.shuffle(button_grid)
         # Actualizamos el contenido de la ventana actual
         pregunta = insertar_salto_de_linea(pregunta_seleccionada[0], 40)
         respuestaCor = insertar_salto_de_linea(pregunta_seleccionada[1], 22)
@@ -93,10 +136,16 @@ def nueva_pregunta():
         respuestaInc2 = insertar_salto_de_linea(pregunta_seleccionada[3], 22)
         respuestaInc3 = insertar_salto_de_linea(pregunta_seleccionada[4], 22)
         preguntaLabel.config(text=pregunta)
-        opcion1.config(text=respuestaCor)
-        opcion2.config(text=respuestaInc1)
-        opcion3.config(text=respuestaInc2)
-        opcion4.config(text=respuestaInc3)
+        opcion1.config(text=respuestaCor, background=button_color[0])
+        opcion2.config(text=respuestaInc1, background=button_color[1])
+        opcion3.config(text=respuestaInc2, background=button_color[2])
+        opcion4.config(text=respuestaInc3, background=button_color[3])
+        opcion1.grid(row=button_grid[0][0], column=button_grid[0][1], sticky="nsew", padx=(button_grid[0][2],button_grid[0][3]))
+        opcion2.grid(row=button_grid[1][0], column=button_grid[1][1], sticky="nsew", padx=(button_grid[1][2],button_grid[1][3]))
+        opcion3.grid(row=button_grid[2][0], column=button_grid[2][1], sticky="nsew", padx=(button_grid[2][2],button_grid[2][3]))
+        opcion4.grid(row=button_grid[3][0], column=button_grid[3][1], sticky="nsew", padx=(button_grid[3][2],button_grid[3][3]))
+        puntaje_label.config(text=f"Puntaje: {puntaje}")
+    
     else:
         fin_juego()
 
@@ -104,7 +153,67 @@ def eliminar_widgets(ventana):
     for widget in ventana.winfo_children():
         widget.destroy()
 
+def empezar_juego():
+    global nombre, telefono, instagram
+    nombre = name_entry.get()
+    instagram = ig_entry.get()
+    telefono = tel_entry.get()
+    if nombre:
+        if len(nombre)>2:
+            #Verifico si alguno de los caracteres de nombre es un número
+            nombre_valido = not any(c.isdigit() for c in nombre)
+        else:
+            messagebox.showwarning("Error", "El nombre debe tener más de 2 caracteres.")
+            return
+    else:
+        messagebox.showwarning("Error", "Debe ingresar un nombre")
+        return
+
+    if nombre_valido:
+        create_window()
+    else:
+        messagebox.showwarning("Error", "El nombre no puede contener números")
+        return
+
+def mostrar_respuesta(window, op):
+    for widget in window.winfo_children():
+        widget.hide()
+    for row in range(18):
+        for column in range(4):
+            crear_celda(window, row, column, (screen_width-180)/4, "red")
+
+    respuesta_label = tk.Label(window, font=(font, 40))
+    respuesta_label.grid.grid(row=7,column=0,columnspan=4)
+ 
+    if op == 1:
+
+        respuesta_label.config(text="Respuesta correcta!", bg="green")
+
+        # Esperar dos segundos
+        time.sleep(2)
+
+        # Ocultar el widget Label
+        respuesta_label.destroy()
+
+    elif op == 2:
+        respuesta_label.config(text="Respuesta incorrecta!", bg="red")
+
+        # Esperar dos segundos
+        time.sleep(2)
+
+        # Ocultar el widget Label
+        respuesta_label.destroy()
+    
+    for widget in window.winfo_children():
+        widget.show()
+
+    nueva_pregunta()
+
+
+
+
 def fin_juego():
+    global tiempo_logrado
     bgcolor = "#4a4949"
     tiempo_logrado = time_label.cget('text')
     reiniciar_tiempo(time_label)
@@ -112,8 +221,11 @@ def fin_juego():
     for row in range(18):
         for column in range(4):
             crear_celda(window, row, column, (screen_width-180)/4, bgcolor)
-    fin_label = tk.Label(window, text="Felicidades! Finalizaste el juego.", font=(font,60))
+    fin_label = tk.Label(window, text=f"Felicidades {nombre.capitalize()}! Finalizaste el juego.", font=(font,60))
     fin_label.grid(row=3,column=0,columnspan=4)
+
+    preguntasT_label = tk.Label(window, font=(font,40), text=f"Contestaste correctamente {totalAcertado} de {totalPreguntas} preguntas")
+    preguntasT_label.grid(row=5,column=0,columnspan=4)
 
     punt_label = tk.Label(window, text=f"Tu puntaje fue: {puntaje}", font=(font,40))
     punt_label.grid(row=6,column=0,columnspan=4)
@@ -122,32 +234,34 @@ def fin_juego():
     tiempoT_label.grid(row=7,column=0,columnspan=4)
 
     regresar_button = tk.Button(window, text="Regresar", cursor="hand2", bg="green", font=(font,20), relief="flat", command=lambda:[main()])
-    regresar_button.grid(row=10,column=0,columnspan=4)
+    regresar_button.grid(row=10,column=0,columnspan=4, ipadx=10, ipady=10)
+
+    guardar_jugador()
 
 
 def create_window():
-    global font, time_label, preguntaLabel, opcion1, opcion2, opcion3, opcion4
+    global font, time_label, preguntaLabel, opcion1, opcion2, opcion3, opcion4, user_label, puntaje_label
 
     bgcolor = "#4a4949"
 
     eliminar_widgets(window)
     
-    bg_colors = ["purple", "red", "blue", "yellow"]
-    bgcolor_pregunta = random.choice(bg_colors)
-    rojo = "#f54254"
-    azul = "#4287f5"
-    verde = "#42f57b"
-    amarillo = "#f5e342"
-    button_color = [rojo, azul, verde, amarillo]
-    random.shuffle(button_color)
-
-    #fila, columna, padx(primervalor), padx(segundovalor)
-    button_grid = [(9,1,0,50),(9,2,50,0),(12,1,0,50),(12,2,50,0)]
-    random.shuffle(button_grid)
     window.config(bg=bgcolor)
     for row in range(18):
         for column in range(4):
             crear_celda(window, row, column, (screen_width-180)/4, bgcolor)
+
+    user_frame = tk.Frame(window, bg="orange")
+    user_frame.grid(row=1,column=0,sticky="nsew", rowspan=4, padx=(30,80), pady=25)
+    user_frame.columnconfigure(0, weight=1)  # Expande la columna 0
+    user_frame.rowconfigure(0, weight=1)  # Expande la fila 0
+    user_frame.rowconfigure(1, weight=1)  # Expande la fila 1
+
+    user_label = tk.Label(user_frame, font=(font, 20), text=f"Jugador: {nombre}", bg="orange")
+    user_label.grid(row=0, column=0, sticky="w", padx=(10,0))
+
+    puntaje_label = tk.Label(user_frame, font=(font, 20), bg="orange")
+    puntaje_label.grid(row=1, column=0, sticky="w", padx=(10,0))
 
     time_frame = tk.Frame(window, bg="#4a4949")
     time_frame.grid(row=1,column=3,sticky="nsew", rowspan=4)
@@ -166,14 +280,10 @@ def create_window():
     pregunta_frame.columnconfigure(0, weight=1)  # Expande la columna 0
     preguntaLabel = tk.Label(pregunta_frame, background="white", font=(font, 30))
     preguntaLabel.grid(row=0,column=0, sticky="nsew")
-    opcion1 = tk.Button(window, width=20, bg="lightgray", cursor="hand2", relief="flat", font=(font, 20), background=button_color[0], command=lambda:[sumar_puntos(), nueva_pregunta()])
-    opcion1.grid(row=button_grid[0][0], column=button_grid[0][1], sticky="nsew", padx=(button_grid[0][2],button_grid[0][3]))
-    opcion2 = tk.Button(window, width=20, bg="lightgray", cursor="hand2", relief="flat", font=(font, 20), background=button_color[1], command=lambda:[nueva_pregunta()])
-    opcion2.grid(row=button_grid[1][0], column=button_grid[1][1], sticky="nsew", padx=(button_grid[1][2],button_grid[1][3]))
-    opcion3 = tk.Button(window, width=20, bg="lightgray", cursor="hand2", relief="flat", font=(font, 20), background=button_color[2], command=lambda:[nueva_pregunta()])
-    opcion3.grid(row=button_grid[2][0], column=button_grid[2][1], sticky="nsew", padx=(button_grid[2][2],button_grid[2][3]))
-    opcion4= tk.Button(window, width=20, bg="lightgray", cursor="hand2", relief="flat", font=(font, 20), background=button_color[3], command=lambda:[nueva_pregunta()])
-    opcion4.grid(row=button_grid[3][0], column=button_grid[3][1], sticky="nsew", padx=(button_grid[3][2],button_grid[3][3]))
+    opcion1 = tk.Button(window, width=20, bg="lightgray", cursor="hand2", relief="flat", font=(font, 20), command=lambda:[sumar_puntos(), mostrar_respuesta(window,1)])
+    opcion2 = tk.Button(window, width=20, bg="lightgray", cursor="hand2", relief="flat", font=(font, 20), command=lambda:[mostrar_respuesta(window,2)])
+    opcion3 = tk.Button(window, width=20, bg="lightgray", cursor="hand2", relief="flat", font=(font, 20), command=lambda:[ mostrar_respuesta(window,2)])
+    opcion4= tk.Button(window, width=20, bg="lightgray", cursor="hand2", relief="flat", font=(font, 20), command=lambda:[ mostrar_respuesta(window,2)])
         
     exitP_button = tk.Button(window, width=20, text="SALIR",bg="#eb6781",cursor="hand2",font=(font, 16), relief="flat", command=lambda:[reiniciar_tiempo(time_label), main()])
     exitP_button.grid(row=15,column=0, rowspan=2)
@@ -182,7 +292,7 @@ def create_window():
 
 
 def main():
-    global font, listaPreguntas, window, screen_width
+    global window, listaPreguntas, totalPreguntas, screen_width, font, totalAcertado, puntaje, tiempo_transcurrido, name_entry, ig_entry, tel_entry
 
     eliminar_widgets(window)
     screen_width = window.winfo_screenwidth()
@@ -204,6 +314,7 @@ def main():
     estilo.configure("TLabel", font=(font, 30)) 
     estilo.configure("TEntry", font=(font, 20)) 
     estilo.configure("Custom.TLabel", font=(font,20))
+    estilo.configure("Title.TLabel", font=(font,45))
 
     window.columnconfigure(0, weight=1)  # Expande la primera columna
     window.columnconfigure(1, weight=1)  # Expande la segunda columna
@@ -233,14 +344,14 @@ def main():
     logo = tk.Label(window, image=icon, background=bgcolor)
     logo.grid(row=0,column=0,sticky="w", rowspan=2, pady=(10,0), padx=(10,0))
 
-    title = ttk.Label(text="TRIVIAUI ", background=bgcolor)
-    title.grid(row=0,column=0, columnspan=2, rowspan=2, padx=(100,0))
+    title = ttk.Label(text="TRIVIAUI ", background=bgcolor, style="Title.TLabel")
+    title.grid(row=0,column=0, columnspan=2, rowspan=3, padx=(100,0), sticky="s", pady=(0,10))
 
     description = ttk.Label(text="EL JUEGO DE PREGUNTAS Y RESPUESTAS DE LOS 40 AÑOS DEL ISAUI.",style="Custom.TLabel", background=bgcolor)
     description.grid(row=17,column=0, columnspan=4, sticky="w", padx=(10,0))
 
     form = tk.Frame(window, bg="lightblue", width=450)
-    form.grid(row=3,column=0,columnspan=2, rowspan=12, sticky="ns", padx=(100,0))
+    form.grid(row=3,column=0,columnspan=2, rowspan=13, sticky="ns", padx=(100,0))
 
     name_label = ttk.Label(text="NOMBRE *", background="lightblue", font=(font, 20))
     name_label.grid(row=4, column=0, columnspan=2, rowspan=2, padx=(100,0), sticky="n")
@@ -260,11 +371,11 @@ def main():
     tel_entry = ttk.Entry(width=25, font=(font, 16))
     tel_entry.grid(row=9,column=0, columnspan=2, rowspan=2, padx=(100,0), sticky="s")
 
-    newGame_button = tk.Button(window, width=20, text="NUEVO JUEGO", bg="#5bd0f0",cursor="hand2",font=(font, 16), relief="flat", command=lambda:[create_window()])
+    newGame_button = tk.Button(window, width=20, text="NUEVO JUEGO", bg="#5bd0f0",cursor="hand2",font=(font, 16), relief="flat", command=lambda:[empezar_juego()])
     newGame_button.grid(row=12,column=0, columnspan=2, rowspan=2, padx=(100,0), sticky="n")
 
     exit_button = tk.Button(window, width=20, text="SALIR",bg="#eb6781",cursor="hand2",font=(font, 16), relief="flat", command=lambda:window.destroy())
-    exit_button.grid(row=13,column=0, columnspan=2, rowspan=2, padx=(100,0))
+    exit_button.grid(row=13,column=0, columnspan=2, rowspan=2, padx=(100,0), sticky="s")
 
     punt_label = ttk.Label(window, text="PUNTAJES", background=bgcolor)
     punt_label.grid(row=1, column=2, columnspan=2, rowspan=2)
@@ -294,14 +405,18 @@ def main():
       
     cargar_jugadores(punt_tree)
 
+    totalAcertado = 0
+    puntaje = 0
+    tiempo_transcurrido = 0
+
     listaPreguntas = cargar_preguntas()
+
+    totalPreguntas = len(listaPreguntas)
 
     window.mainloop()
 
 if __name__ == "__main__":
     conexion = mysql.connector.connect(host="localhost", user="root", password="", database="trivia")
-    puntaje = 0
-    tiempo_transcurrido = 0
     font = "Gill Sans MT"
     listaPreguntas = []
     window = tk.Tk()
